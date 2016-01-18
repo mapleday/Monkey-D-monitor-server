8f838492323efc0f42446b1b20945c421f3c37f9
chenshouqin <shouqinchen@sohu-inc.com>
2016-01-18 17:08:02 +0800

    注释statLog收集数据代码

--- src/main/java/com/sohu/sns/monitor/timer/StatLogCollector.java
+++ src/main/java/com/sohu/sns/monitor/timer/StatLogCollector.java
@@ -1,23 +1,16 @@
 package com.sohu.sns.monitor.timer;
 
 import com.sohu.sns.monitor.model.StatLogInfo;
-import com.sohu.sns.monitor.util.DateUtil;
-import com.sohu.sns.monitor.util.MathsUtils;
 import com.sohu.snscommon.dbcluster.service.MysqlClusterService;
 import com.sohu.snscommon.dbcluster.service.exception.MysqlClusterException;
-import com.sohu.snscommon.utils.LOGGER;
-import com.sohu.snscommon.utils.constant.ModuleEnum;
-import com.sohu.snscommon.utils.http.HttpClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.util.*;
 
 /**
  * Created by Gary on 2015/12/15.
@@ -55,7 +48,7 @@ public class StatLogCollector {
 
     public void handle() throws MysqlClusterException, ParseException {
 
-        System.out.println("statLog collector begin ...  time : " + DateUtil.getCurrentTime());
+ /*       System.out.println("statLog collector begin ...  time : " + DateUtil.getCurrentTime());
         JdbcTemplate readJdbcTemplate = mysqlClusterService.getReadJdbcTemplate(null);
         JdbcTemplate writeJdbcTemplate = mysqlClusterService.getWriteJdbcTemplate(null);
         String beginTime = DateUtil.getBeforeCurrentHour(0);
@@ -73,16 +66,16 @@ public class StatLogCollector {
                 Long countByDay = readJdbcTemplate.queryForObject(QUERY_IS_EXIST_BYDAY, Long.class, statLogInfo.getAppId(),
                         statLogInfo.getModuleName(), statLogInfo.getMethodName(), currentDate);
                 if(0 == countByDay) {
-                    /** 插入新的统计*/
+                    *//** 插入新的统计*//*
                     writeJdbcTemplate.update(INSERT_STAT_LOG_BYDAY, statLogInfo.getAppId(), statLogInfo.getModuleName(), statLogInfo.getMethodName(),
                             statLogInfo.getVisitCount(), statLogInfo.getTimeoutCount(), currentDate);
                 } else {
-                    /** 更新已经存在的记录*/
+                    *//** 更新已经存在的记录*//*
                     writeJdbcTemplate.update(UPDATE_STAT_LOG_BYDAY, statLogInfo.getVisitCount(), statLogInfo.getTimeoutCount(), statLogInfo.getAppId(),
                             statLogInfo.getModuleName(), statLogInfo.getMethodName(), currentDate);
                 }
 
-                /**预测是不是访问异常**/
+                *//**预测是不是访问异常**//*
                 List<Integer> visitCountList = readJdbcTemplate.query(QUERY_HISTORY_VISITCOUNT, new AlarmVisitCountMapper(), statLogInfo.getAppId(),
                         statLogInfo.getModuleName(), statLogInfo.getMethodName(), beforeCurrentHour, beginDate);
                 Collections.sort(visitCountList);
@@ -94,13 +87,13 @@ public class StatLogCollector {
                     }
                 }
 
-                /**插入每个小时的记录**/
+                *//**插入每个小时的记录**//*
                 writeJdbcTemplate.update(INSERT_STAT_LOG_BYHOUR, statLogInfo.getAppId(), statLogInfo.getModuleName(), statLogInfo.getMethodName(),
                         statLogInfo.getInstanceNum(), statLogInfo.getVisitCount(), statLogInfo.getTimeoutCount(),
                         statLogInfo.getAllCompileTime()/statLogInfo.getVisitCount(), beforeCurrentHour, currentDate);
             }
 
-            /**发送告警邮件**/
+            *//**发送告警邮件**//*
             if(0 != sb.length()) {
                 String content = String.format(EMAIL_CONTENT, currentDate, beforeCurrentHour) + sb.toString();
                 Map<String, String> map = new HashMap<String, String>();
@@ -115,7 +108,7 @@ public class StatLogCollector {
         } finally {
             writeJdbcTemplate.update(DELETE_RECORD, endTime);
         }
-
+*/
     }
 
     private class StormResultMapper implements RowMapper {

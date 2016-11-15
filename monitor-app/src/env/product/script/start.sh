15dc511d9e2d9c163698b2656ca393e95100e642
jinyingshi <jinyingshi@sohu-inc.com>
2016-11-15 19:41:00 +0800

    启动脚本

--- monitor-app/src/env/product/script/start.sh
+++ monitor-app/src/env/product/script/start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-ZK_CONF=$USER_DIR/app/config/zk.json
-PORT=8080
+ZK_CONF="$USER_DIR/app/config/zk.json"
+
+
 
 APP_MAINCLASS=com.sohu.sns.monitor.app.APP
+
+AGENT_APP_ID=$APPID\_$INSTANCEID
 
 CLASSPATH='.'
 for i in /opt/src/app/lib/*.jar; do
@@ -15,15 +18,15 @@ JAVA_OPTS=" -server -Xms6114m -Xmx6114m -Xmn2200m -Xss256k
 -XX:HeapDumpPath=/opt/logs/
 -Djava.awt.headless=true
 -XX:MaxPermSize=512m
-
--Dcom.sun.management.jmxremote.port=11099
+-Dfile.encoding=UTF-8
+-Dcom.sun.management.jmxremote.port=12328
 -Dcom.sun.management.jmxremote.authenticate=false
 -Dcom.sun.management.jmxremote.ssl=false
 -Dcom.sun.management.jmxremote.password=false
 -Dcom.sun.management.jmxremote
--Dfile.encoding=UTF-8
+-Dmonitor_agent_app_id=$AGENT_APP_ID
 -Denv=TEST"
 
-JAVA_CMD="$JAVA_HOME/bin/java $JAVA_OPTS -Dlogback.configurationFile=$USER_DIR/app/config/logback.xml -classpath $CLASSPATH $APP_MAINCLASS $ZK_CONF $APPID"
+JAVA_CMD="$JAVA_HOME/bin/java $MONITOR_OPTS $JAVA_OPTS -Dlogback.configurationFile=$USER_DIR/config/logback.xml -classpath $CLASSPATH $APP_MAINCLASS $ZK_CONF $APPID"
 echo $JAVA_CMD
 $JAVA_CMD

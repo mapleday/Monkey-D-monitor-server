bddc3e8ce35d2e5a487e9416e700ebde6ded66d5
liucong <congliu002405@sohu-inc.com>
2017-02-09 13:49:36 +0800

    -增加qps峰值统计-

--- es-monitor/src/main/java/com/sohu/sns/monitor/es/schedule/EsSchedule.java
+++ es-monitor/src/main/java/com/sohu/sns/monitor/es/schedule/EsSchedule.java
@@ -8,15 +8,22 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramBuilder;
+import org.elasticsearch.search.aggregations.bucket.histogram.DateHistogramInterval;
+import org.elasticsearch.search.aggregations.bucket.histogram.Histogram;
+import org.elasticsearch.search.aggregations.bucket.histogram.InternalHistogram;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.elasticsearch.search.aggregations.metrics.avg.AvgBuilder;
 import org.elasticsearch.search.aggregations.metrics.avg.InternalAvg;
+import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 
 
 import java.text.DecimalFormat;
@@ -106,11 +113,20 @@ public class EsSchedule {
         speInterfaceUri.add("/v6/feeds/profile/template");
         speInterfaceUri.add("/v6/feeds/timeline/template");
         Set<EsResult> orderResults = new TreeSet();
+        StringBuilder sb = new StringBuilder().append("QPS统计 \n");
 
         Map<String, EsResult> monitorResults = queryEs(monitorTime, endTime);
         System.out.println(monitorResults);
         //总qps 统计进去。
         EsResult sumResult = new EsResult();
+
+        //qps峰值
+        InternalHistogram.Bucket bucket = queryQpsMax(monitorTime, endTime);
+        SimpleDateFormat dateformat=new SimpleDateFormat("HH:mm:ss");
+        DateTime maxKey = (DateTime) bucket.getKey();
+        sb.append("峰值:   "+bucket.getDocCount()+"    "+dateformat.format(new Date(maxKey.getMillis())).toString()+"\n");
+
+
         sumResult.setInterfaceUri("总计：");
         for (Map.Entry<String, EsResult> entry : monitorResults.entrySet()) {
             EsResult result = entry.getValue();
@@ -126,7 +142,10 @@ public class EsSchedule {
         }
 
         orderResults.add(sumResult);
-        StringBuilder sb = new StringBuilder().append("QPS统计 \n");
+
+
+        DecimalFormat decimalFormat = new DecimalFormat("0.000");
+
         for (EsResult result : orderResults) {
 
             //定期更新
@@ -135,25 +154,33 @@ public class EsSchedule {
             }
 
             String key = result.getInterfaceUri();
-            DecimalFormat decimalFormat = new DecimalFormat("0.000");
+
             double qps = Double.parseDouble(decimalFormat.format(result.getQps()));
             double avgTime = result.getAvgTime();
+
             sb.append(key + " qps:" + qps + " avg:" + avgTime + " total：" + result.getTotoalCount() + " \n ");
+
+
         }
-        sb.append("\n 详情:");
+        //sb.append("\n 详情:");
         System.out.println(sb.toString());
         notifyService.sendAllNotifyPerson(sb.toString());
     }
 
     private Map<String, EsResult> queryEs(Date startTime, Date endTime) {
+
         TermsBuilder interfaceCount = AggregationBuilders.terms("interfaceCount")
                 .field("interface.raw")
                 .size(300)
                 .order(Terms.Order.count(true));
+
         AvgBuilder interfaceAvgTime = AggregationBuilders.avg("interfaceAvgTime").field("request_time");
+
         QueryBuilder qb = QueryBuilders.boolQuery()
                 .must(QueryBuilders.rangeQuery("@timestamp").gte(startTime).lte(endTime))
                 .must(QueryBuilders.regexpQuery("interface.raw", "/v[56]/.*"));
+
+
         SearchResponse searchResponse = client.prepareSearch("logstash-detail-msapi*")
                 .setQuery(qb)
                 .addAggregation(interfaceCount.subAggregation(interfaceAvgTime))
@@ -164,6 +191,7 @@ public class EsSchedule {
         StringTerms interfaceCountAggr = (StringTerms) searchResponse.getAggregations().asMap().get("interfaceCount");
         List<Terms.Bucket> buckets = interfaceCountAggr.getBuckets();
         Map<String, EsResult> results = new HashMap();
+
         for (Terms.Bucket bucket : buckets) {
             long count = bucket.getDocCount();
             String uri = bucket.getKey().toString();
@@ -180,7 +208,34 @@ public class EsSchedule {
             results.put(uri, esResult);
         }
 
+
         return results;
+    }
+
+    private InternalHistogram.Bucket queryQpsMax(Date startTime, Date endTime) {
+
+        // 查询QPS峰值
+        QueryBuilder qb1 = QueryBuilders.boolQuery()
+                .must(QueryBuilders.rangeQuery("@timestamp").gte(startTime).lte(endTime))
+                .must(QueryBuilders.regexpQuery("interface.raw", "/v[56]/.*"))
+                .mustNot(QueryBuilders.regexpQuery("interface.raw","/v6/topic/feed/repost_count")) ;
+
+        DateHistogramBuilder qpsDate = AggregationBuilders.dateHistogram("timestamp").field("@timestamp").order(Histogram.Order.COUNT_DESC)
+                .interval(DateHistogramInterval.seconds(1)).minDocCount(1).timeZone("Asia/Shanghai");
+
+        SearchResponse searchResponse1 = client.prepareSearch("logstash-detail*")
+                .setQuery(qb1)
+                .setSize(1)
+                .addAggregation(qpsDate)
+                .execute()
+                .actionGet();
+
+        InternalHistogram internalHistogram = (InternalHistogram) searchResponse1.getAggregations().asList().get(0);
+
+        InternalHistogram.Bucket bucket = (InternalHistogram.Bucket)internalHistogram.getBuckets().get(0);
+
+        return bucket;
+
     }
 
     public static void main(String[] args) {

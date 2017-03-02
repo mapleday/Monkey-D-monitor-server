6bed1791c2ec0a12c099ca5dbb6f56d5a15eaa7c
yuanwei <mapleday@126.com>
2017-03-02 20:27:50 +0800

    onlytest

--- monitor-web/src/main/java/com/sohu/sns/monitor/web/service/PersonDutyService/SelectPersonDutyService.java
+++ monitor-web/src/main/java/com/sohu/sns/monitor/web/service/PersonDutyService/SelectPersonDutyService.java
@@ -32,7 +32,7 @@ public class SelectPersonDutyService {
         SelectPersonDutyService.notifyService=notifyService;
     }
 
-    @Scheduled(cron = "0 0 19 * * ?")
+//    @Scheduled(cron = "0 0 19 * * ?")
     public  static void sendDutyInfo()  {
             /**发送值班提醒邮件和短信**/
 
